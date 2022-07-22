18d3a7cd61879949a37dfb710d4cb440dc89db69
RealWorldd <jinpark99@naver.com>
2022-07-23 02:48:54 +0900

    feat: 카카오 로그인 기능 추가

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -49,50 +49,61 @@ export const signin = async (req, res, next) => {
   }
 };
 
-export const loginWithKakao = async (req, res) => {
-  const host = "https://kauth.kakao.com/oauth/token";
-  const config = {
-    grant_type: "authorization_code",
-    client_id: process.env.KAKAO_KEY,
-    redirect_uri: "http://localhost:3000/join/oauth/kakao",
-    code: req.body.code,
-    client_secret: process.env.KAKAO_SECRET,
-  };
-  const params = new URLSearchParams(config).toString();
-  const tokenRequestUrl = `${host}?${params}`;
-  const tokenRequset = await (
-    await fetch(tokenRequestUrl, {
-      method: "POST",
-      headers: {
-        Content_type: "application/json",
-      },
-    })
-  ).json();
-  if ("access_token" in tokenRequset) {
-    const { access_token } = tokenRequset;
-    const url = "https://kapi.kakao.com/v2/user/me";
-    const userData = await (
-      await fetch(url, {
+export const loginWithKakao = async (req, res, next) => {
+  try {
+    const host = "https://kauth.kakao.com/oauth/token";
+    const config = {
+      grant_type: "authorization_code",
+      client_id: process.env.KAKAO_KEY,
+      redirect_uri: "http://localhost:3000/join/oauth/kakao",
+      code: req.body.code,
+      client_secret: process.env.KAKAO_SECRET,
+    };
+    const params = new URLSearchParams(config).toString();
+    const tokenRequestUrl = `${host}?${params}`;
+    const tokenRequset = await (
+      await fetch(tokenRequestUrl, {
+        method: "POST",
         headers: {
-          Authorization: `Bearer ${access_token}`,
+          Content_type: "application/json",
         },
       })
     ).json();
-    console.log(userData);
-    console.log(userData.id);
-    console.log(userData.kakao_account.profile.nickname);
-  //   await User.create({
-  //     email: userData.email,
-  //     password: "",
-  //     name: userData.name,
-  //     fromKakao: true,
-  //   });
-  // } else {
-    res
-      .cookie("access_token", token, {
-        httpOnly: true,
-      })
-      .status(200)
-      .json(userData);
+    if ("access_token" in tokenRequset) {
+      const { access_token } = tokenRequset;
+      const url = "https://kapi.kakao.com/v2/user/me";
+      const userData = await (
+        await fetch(url, {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        })
+      ).json();
+      console.log(userData);
+      if (!userData.kakao_account.has_email) {
+        return next(createError(404, "이메일이 존재하지 않습니다."));
+      }
+      let user = await User.findOne({ email: userData.kakao_account.email });
+      if (!user) {
+        user = await User.create({
+          email: userData.kakao_account.email,
+          password: "",
+          img: userData.properties.profile_image,
+          name: userData.kakao_account.profile.nickname,
+          fromKakao: true,
+        });
+      }
+      const token = jwt.sign({ id: user._id }, process.env.JWT);
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .status(200)
+        .json(user);
+    } else {
+      return next(createError(404, "Access Token이 존재하지 않습니다."));
+    }
+  } catch (err) {
+    next(err);
   }
 };

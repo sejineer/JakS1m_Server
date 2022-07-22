2091c4c6feb0737a6d4f13644297647d132501ae
RealWorldd <jinpark99@naver.com>
2022-07-23 01:50:16 +0900

    test

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const logger = morgan("dev");
 app.use(logger);
 app.use(cors());
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
 
